16914b53f91faf15c95a3b8a400685d5c7491a56
Drupal Aid Support <support@drupalaid.com>
2019-06-19 20:41:31 +0000

    .gitignore is now working

--- sites/all/modules/custom/ioby_faq/theme/ioby-faq-page.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php print theme('links', array(
-  'links' => $category_links,
-  'attributes' => array('class' => 'tabs'),
-  )); ?>
-<?php print $view_display; ?>
--- sites/all/themes/iobytheme/search-result--apachesolr_search--node--project_2.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$node_output = node_view(node_load($result['node']->entity_id), 'teaser');
-print drupal_render($node_output);
